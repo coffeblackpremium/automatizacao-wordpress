9cd7cf828c43d4f31faaf86bcf8414606161d60e
Your Name <joaodamaceno188@gmail.com>
2022-05-13 00:02:59 -0300

    Modificado os method antigo 'find_element' e resolvido o problema do mouse

--- main.py
+++ main.py
@@ -4,35 +4,26 @@ from xml.dom.minidom import Element
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 import time
 import win32clipboard
 
 def efetuar_login(driver):
     try:
-        login = driver.find_element_by_id('user_login')
-        password = driver.find_element_by_id("user_pass")
-        login_submit = driver.find_element_by_id("wp-submit")
-        
-        password.send_keys("pro80of")
-        login.send_keys("producao")
-        login_submit.click()
+        login = driver.find_element(By.ID, 'user_login').send_keys("producao")
+        password = driver.find_element(By.ID, "user_pass").send_keys("pro80of")
+        login_submit = driver.find_element(By.ID, "wp-submit").click()
         time.sleep(5)
     except:
         #Tratar futuros excepts
         pass
-"""
-def copy_text_for_clipboard(driver):
-    driver.send_keys(Keys.CONTROL, 'a')
-    driver.send_keys(Keys.CONTROL, 'c')
-    win32clipboard.OpenClipboard()
-    text = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
-    return text
-"""
+
 def add_tags(driver):
-    title = driver.find_elements_by_xpath("//input[@class='ptitle']")
-    get_tag = driver.find_element_by_xpath("//textarea[@class='tax_input_list-tags ui-autocomplete-input']")
-    button_atualizar = driver.find_elements_by_xpath("//button[@class='button button-primary save alignright']")[0]
+    title = driver.find_elements(By.XPATH, "//input[@class='ptitle']")
+    get_tag = driver.find_element(By.XPATH, "//textarea[@class='tax_input_list-tags ui-autocomplete-input']")
+    button_atualizar = driver.find_elements(By.XPATH, "//button[@class='button button-primary save alignright']")[0]
     get_title = title[0]
     if get_title:
         get_title.send_keys(Keys.CONTROL, 'a')
@@ -40,6 +31,7 @@ def add_tags(driver):
         win32clipboard.OpenClipboard()
         text = win32clipboard.GetClipboardData()
         win32clipboard.CloseClipboard()
+        time.sleep(2)
         get_tag.send_keys(Keys.CONTROL, 'v')
         button_atualizar.click()
         
@@ -49,14 +41,15 @@ driver = webdriver.Chrome(PATH)
 print(driver.title)
 driver.get("http://obrasflex.com.br/wp-admin/edit.php?post_type=listing")
 efetuar_login(driver=driver)
-source_html = driver.page_source
-posts = []
-posts = driver.find_elements_by_xpath("//button[@class='button-link editinline']")
-
+posts = driver.find_elements(By.XPATH, "//button[@class='button-link editinline']")
+#posts = WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//button[@class='button-link editinline']")))
+#text = WebDriverWait(driver, 2).until(expected_conditions.presence_of_element_located((By.XPATH, "//a[@class='row-title']")))
+text = driver.find_elements(By.XPATH, "//a[@class='row-title']")
 for count in range(0, 600):
     post_counts = posts[count]
     if post_counts:
-        click_posting = post_counts
+        click_posting = posts[count]
+        hover = ActionChains(driver).move_to_element(text[count]).perform()
         driver.implicitly_wait(10)
         click_posting.click()
         add_tags(driver=driver)
